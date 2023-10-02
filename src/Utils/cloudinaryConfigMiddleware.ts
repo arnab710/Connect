c5f5300c4d6a563ce0c1b15461ca4cff2cae91cd
Arnab Dey <arnabdey710@gmail.com>
2023-10-02 17:28:15 +0530

    unknown change

--- src/Utils/cloudinaryConfigMiddleware.ts
+++ src/Utils/cloudinaryConfigMiddleware.ts
@@ -6,6 +6,22 @@ import path from "path";
 export const cloudinaryConfig = async (req: Request, res: Response, next: NextFunction) => {
 	if (!req.file) return next();
 	try {
+		const buffer = req.file.buffer;
+		const fileType = req.body?.fileType;
+
+		let transformation;
+		if (req.file.mimetype.startsWith("image") && fileType === "profile-picture") {
+			transformation = { width: 500, height: 500, crop: "fill" };
+		} else if (req.file.mimetype.startsWith("image") && fileType === "cover-photo") {
+			transformation = { width: 820, height: 312, crop: "fill" };
+		} else if (req.file.mimetype.startsWith("image") && fileType === "image-post") {
+			transformation = { width: 700, height: 800, crop: "fill" };
+		} else if (req.file.mimetype.startsWith("video") && fileType === "video") {
+			transformation = { width: 600, height: 750, crop: "fill", eager: [{ width: 600, height: 750, crop: "fill" }], resource_type: "video", chunk_size: 2000000, eager_async: true };
+		} else if (req.file.mimetype.startsWith("audio") && fileType === "audio") {
+			transformation = { transformation: [{ audio_frequency: 8000 }, { audio_codec: "mp3" }], resource_type: "video", chunk_size: 1000000 };
+		}
+
 		let result: any;
 		const normalized_path = path.normalize(req.file.path).replace(/\\/g, "/");
 		//uploading to cloudinary
--- src/Utils/multerConfig.ts
+++ src/Utils/multerConfig.ts
@@ -2,19 +2,6 @@ import { Request } from "express";
 import multer from "multer";
 import customError from "./customError";
 
-const multerStorage = multer.diskStorage({
-	destination: (_req: Request, file: Express.Multer.File, cb) => {
-		if (file.mimetype.split("/")[0] === "image") cb(null, "public/assets/images");
-		else if (file.mimetype.split("/")[0] === "video") cb(null, "public/assets/videos");
-		else if (file.mimetype.startsWith("audio")) cb(null, "public/assets/audios");
-		else cb(new customError(400, "file type not supported"), "");
-	},
-	filename: (req: any, file: Express.Multer.File, cb) => {
-		const ext = file.mimetype.split("/")[1];
-		cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
-	},
-});
-
 const multerFilter = (_req: Request, file: Express.Multer.File, cb: any) => {
 	if (file.mimetype?.startsWith("image") || file.mimetype?.startsWith("video") || file.mimetype?.startsWith("audio")) cb(null, true);
 	else {
@@ -24,7 +11,7 @@ const multerFilter = (_req: Request, file: Express.Multer.File, cb: any) => {
 };
 
 const upload = multer({
-	storage: multerStorage,
+	storage: multer.memoryStorage(),
 	fileFilter: multerFilter,
 	limits: { fileSize: 7 * 1024 * 1024 },
 });
